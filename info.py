1291bd526a743d9552d68cf4d546f7bb512e5b33
fileuplaoderbot <tparveen2728@gmail.com>
2024-12-10 13:19:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7574798369:AAFdxLM4a5rXXO34GtZyyorwGPmlN3y
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6069621485').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Badmosh_bhai") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002489194899'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002320799080'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+wkLciI8DXmVlNTU9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002409173444').split()]
 #---------------------------------------------------------------
